3ee2ddb2a8b3c50de8ccbc554acd6e295ac78930
Harsh Kumar <harshpatel9811838836@gmail.com>
2023-08-19 13:34:06 +0530

    Adding all files

---
+++ script.js
@@ -0,0 +1,18 @@
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?type=single";
+
+let getJoke = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      jokeContainer.innerHTML = item.joke;
+    });
+};
+
+btn.addEventListener("click", getJoke);
+
+let toogler = document.querySelector(".toggler");
+toogler.addEventListener("click", () => {
+  document.body.classList.toggle("active");
+});
